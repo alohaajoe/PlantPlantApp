54f632ed5d0e069d3d758d6c6a7a9c9b0cd7a34f
jhaseloh <joe.haseloh@gmail.com>
2024-11-08 00:41:46 +0100

    requests every 10 seconds

--- App.js
+++ App.js
@@ -5,24 +5,34 @@ import React, { useEffect, useState } from 'react';
 export default function App() {
   const [data, setData] = useState(null);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://192.168.178.38:8000/api', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log('Fetched data:', data);
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   useEffect(() => {
-    fetch('http://192.168.178.38:8000/api', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Fetched data:', data);
-        setData(data);
-      })
-      .catch(error => console.error('Error fetching data:', error));
+    // Initialer Datenabruf
+    fetchData();
+
+    // Intervall für wiederholten Datenabruf
+    const intervalId = setInterval(fetchData, 10000); // 10000 ms = 10 Sekunden
+
+    // Cleanup-Funktion, um das Intervall zu löschen, wenn die Komponente unmontiert wird
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
