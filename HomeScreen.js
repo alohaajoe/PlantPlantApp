d0ade2419079d1e9fe477e5ca074bc72cca1e072
jhaseloh <joe.haseloh@gmail.com>
2025-02-17 17:00:49 +0100

    got the new pictures but somehow it wont show.

--- HomeScreen.js
+++ HomeScreen.js
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import Titel from "./Titel";
 
-const HEARTFULL_IMAGE = require("./assets/healthbar/HeartFull.png");
-const HEARTEMPTY_IMAGE = require("./assets/healthbar/HeartEmpty.png");
-const HEARTHALF_IMAGE = require("./assets/healthbar/HeartHalf.png");
+const HEALTH05_IMAGE = require("./assets/healthbar/Health05.png");
+const HEALTH1_IMAGE = require("./assets/healthbar/Health1.png");
+const HEALTH15_IMAGE = require("./assets/healthbar/Health15.png");
+const HEALTH2_IMAGE = require("./assets/healthbar/Health2.png");
+const HEALTH25_IMAGE = require("./assets/healthbar/Health25.png");
+const HEALTH3_IMAGE = require("./assets/healthbar/Health3.png");
+const HEALTH35_IMAGE = require("./assets/healthbar/Health35.png");
+const HEALTH4_IMAGE = require("./assets/healthbar/Health4.png");
+const HEALTH45_IMAGE = require("./assets/healthbar/Health45.png");
+const HEALTH5_IMAGE = require("./assets/healthbar/Health5.png");
 
 export default function HomeScreen() {
     const [data, setData] = useState(null);
@@ -34,33 +41,49 @@ export default function HomeScreen() {
         }
     };
 
-    const mapDataToImage = () => {
+    const mapDataToHealthbar = () => {
       const number = data?.number;
-      if (0 <= number && number <= 33) {
-        return HEARTEMPTY_IMAGE;
-      } else if (33 < number && number < 66) {
-        return HEARTHALF_IMAGE;
-      } else if (66 <= number && number <= 100) {
-        return HEARTFULL_IMAGE;
+      if (0 <= number && number < 10) {
+        return HEALTH05_IMAGE;
+      } else if (10 <= number && number < 20) {
+        return HEALTH1_IMAGE;
+      } else if (20 <= number && number < 30) {
+        return HEALTH15_IMAGE;
+      } else if (30 <= number && number < 40) {
+        return HEALTH2_IMAGE;
+      } else if (40 <= number && number < 50) {
+        return HEALTH25_IMAGE;
+      } else if (50 <= number && number < 60) {
+        return HEALTH3_IMAGE;
+      } else if (60 <= number && number < 70) {
+        return HEALTH35_IMAGE;
+      } else if (70 <= number && number < 80) {
+        return HEALTH4_IMAGE;
+      } else if (80 <= number && number < 90) {
+        return HEALTH45_IMAGE;
+      } else if (90 <= number && number <= 100) {
+        return HEALTH5_IMAGE;
       }
     }
   
     useEffect(() => {
       // Initialer Datenabruf
       fetchData();
-      mapDataToImage();
+      mapDataToHealthbar();
   
       // Intervall für wiederholten Datenabruf
       const intervalId = setInterval(fetchData, 10000); // 10000 ms = 10 Sekunden
   
       // Cleanup-Funktion, um das Intervall zu löschen, wenn die Komponente unmontiert wird
       return () => clearInterval(intervalId);
+      console.log("Geladenes Bild:", mapDataToHealthbar());
+
     }, []);
 
     return (
       <View style={styles.container}>
       <Titel style={styles.titel}>PlantPlant</Titel>
-      <Image source={mapDataToImage()} style={styles.heartImage} />
+      <Image source={mapDataToHealthbar()} style={styles.heartImage} />
       <Text style={styles.dataText}>Empfangene Zahl: {data?.number ?? 'Keine Daten'}</Text>
       </View>
     );
