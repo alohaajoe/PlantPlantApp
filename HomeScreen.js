0c3d13e0a7e3cbe9bfb6357bd65f11138f0c7e24
jhaseloh <joe.haseloh@gmail.com>
2025-02-17 14:33:03 +0100

    the data is getting mapt to the heart images but only one heart

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import Titel from "./Titel";
+
+const HEARTFULL_IMAGE = require("./assets/healthbar/HeartFull.png");
+const HEARTEMPTY_IMAGE = require("./assets/healthbar/HeartEmpty.png");
+const HEARTHALF_IMAGE = require("./assets/healthbar/HeartHalf.png");
 
 export default function HomeScreen() {
     const [data, setData] = useState(null);
@@ -29,10 +33,22 @@ export default function HomeScreen() {
         setIsLoading(false);
         }
     };
+
+    const mapDataToImage = () => {
+      const number = data?.number;
+      if (0 <= number && number <= 33) {
+        return HEARTEMPTY_IMAGE;
+      } else if (33 < number && number < 66) {
+        return HEARTHALF_IMAGE;
+      } else if (66 <= number && number <= 100) {
+        return HEARTFULL_IMAGE;
+      }
+    }
   
     useEffect(() => {
       // Initialer Datenabruf
       fetchData();
+      mapDataToImage();
   
       // Intervall für wiederholten Datenabruf
       const intervalId = setInterval(fetchData, 10000); // 10000 ms = 10 Sekunden
@@ -43,8 +59,9 @@ export default function HomeScreen() {
 
     return (
       <View style={styles.container}>
-        <Titel style={styles.titel}>PlantPlant</Titel>
-        <Text style={styles.dataText}>Empfangene Zahl: {data?.number ?? 'Keine Daten'}</Text>
+      <Titel style={styles.titel}>PlantPlant</Titel>
+      <Image source={mapDataToImage()} style={styles.heartImage} />
+      <Text style={styles.dataText}>Empfangene Zahl: {data?.number ?? 'Keine Daten'}</Text>
       </View>
     );
 };
@@ -59,6 +76,11 @@ const styles = StyleSheet.create({
   },
   titel: {
     position: 'absolute',
-    top: 125,
+    top: '20%',
+  },
+  heartImage: {
+    width: '14%',
+    aspectRatio: 55 / 51.77,
+    marginBottom: 20,
   },
 });
